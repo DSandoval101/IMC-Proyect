1a722549e964789e0a54acec05ea948b51145971
Daniel Sandoval <122582902+DSandoval101@users.noreply.github.com>
2023-02-04 21:49:10 -0800

    Add files via upload
    Este es el achivo final.

--- main.py
+++ main.py
@@ -3,34 +3,30 @@ print('\n\n\n\t\t\t Bienvenidos a la Calculadora de Indice de Masa Corporal\n')
 
 personas = int(input( "personas: "))
 
-#Aqui verificamos que la cantidad sea mayor a 0 si no, no tiene sentido pedir nada
+#Aqui verificamos que la cantidad sea mayor a 0
 while personas > 0:
 
-    n = input("Introduzca su nombre por favor: ")
-    ln = input('Introduzca su apellido paterno:  ')
-    mn = input('Introduzca su apell1ido materno:  ')
+    n = str(input("Introduzca su nombre por favor: "))
+    ln = str(input('Introduzca su apellido paterno:  '))
+    mn = str(input('Introduzca su apell1ido materno:  '))
    #Se pide al edad que siempre es un entero por eso el int()
     e = int(input("Su edad en años por favor: "))
-    #como la altura es en metros y no centimetros hay que ponerle punto y por ende es un flotante float()
-    a = float(input ("Su altura en metros por favor: "))
+    a = float(input("Su altura en metros por favor: "))
     #Aqui se duplica codigo pero bueno... decimos que est (de estatura) es igual a altura (No me diga)
     est = a
-    #La masa en kilogramos si puede tener decimales asi que la dejamos flotante
-    m = float (input("Su masa en kilogramos por favor :"))
-    #Calculo del IMC, masa (En kilogramos) entre la estatura (En metros) elevada al cuadrado
+    m = float(input("Su masa en kilogramos por favor :"))
+    #Calculo del IMC
     IMC = m / est**2
-    #Le decimos si es menor o mayor de edad, si es menor a 18 es menor, si no es mayor edad
-    #Solo ruegue porque a nadie se le ocurra meter numeros negativos, le va a decir que es menor de edad
     if(e < 18):
         print(n,ln,mn)
         print("Usted es menor de edad")
     else:
         print(n,ln,mn)
         print("Usted es mayor de edad")
-    #Le imprimos el IMC para que se pong
+    #imprimos el IMC
     print("Su IMC: " + str(IMC) )
 
-    #Hacemos las distintas validaciones
+    #Hacemos las validaciones
     if IMC >= 0 and IMC <= 15.99 :
         print ("Delgadez severa")
     elif IMC >= 16.00 and IMC <= 16.99 :
@@ -44,9 +40,9 @@ while personas > 0:
     elif IMC >= 30.00 and IMC <= 34.99:
         print ("obesidad leve")
     elif IMC >= 35.00 and IMC <= 39.00:
-        print ("obesidad media")
+        print ("Obesidad media")
     elif IMC >= 40.00:
-        print ("obesidad morbida")
+        print ("Obesidad morbida")
     personas = personas - 1
     print(n,ln,mn,'Gracias por usar la IMC')
     input('Presione cualquier tecla para terminar')
